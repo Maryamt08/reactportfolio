428d7d71839dac1647c119110fdd420a87b4dcbc
Maryamt08 <73563078+Maryamt08@users.noreply.github.com>
2021-04-21 23:11:27 -0400

    Add files via upload

---
+++ src/components/Footer/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar">
+
+            <h1 className="header-title">My Portfolio</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Footer/Projects/Projects.js
@@ -0,0 +1,130 @@
+import React from "react";
+
+
+export function Projects() {
+    return (
+
+        <div>
+
+            <a href="https://secret-bayou-23553.herokuapp.com/">Project 1</a>
+
+            <a href="https://rocky-caverns-32147.herokuapp.com/">Project 2</a>
+
+            <a href="https://maryamt08.github.io/workPlanner/">Project 3</a>
+
+            <a href="https://maryamt08.github.io/homework3/">Project 4</a>
+
+            <a href="https://maryamt08.github.io/homework4/">Project 5</a>
+
+
+        </div>
+
+
+    );
+}
+
+export default Projects;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*export function Projects() {
+    return (
+        <div className="project-container1">
+            <div className="card">
+                <h1>Google</h1>
+                <p>Everything is here</p>
+            </div>
+        </div>
+    );
+}
+
+export default Projects;*/
+
