0b979072108d048834427773e3f4500d8727d4f2
Maryamt08 <73563078+Maryamt08@users.noreply.github.com>
2021-04-21 23:13:21 -0400

    Add files via upload

---
+++ src/components/Footer/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export function Header() {
+    return (
+        <div className="header-container">        
+            <header className="header">
+    </header>
+    </div>
+
+    );
+}
+
+export default Header;
